3c0efe8ba9634f636cd00fa1bf79595c51f30279
Adam Menges <adammenges@Wolfsburg.local>
2013-03-22 08:47:48 -0600

    Improved environment.bash

--- bashrcs/environment.bash
+++ bashrcs/environment.bash
@@ -1,4 +1,4 @@
-export PATH=$PATH:/usr/local/git/bin/
+export PATH=$PATH:/usr/local/git/bin
 
 # PS1 config
 PS1="\n\[\e[32;1m\](\`if [ \$? = 0 ]; then echo \[\e[33m\]^_^\[\e[0m\]; else echo \[\e[31m\]X_X\[\e[0m\]; fi\`\[\e[32;1m\]) - \[\e[32;1m\](\[\e[37;1m\]\u@\h\[\e[32;1m\]) - (\[\e[37;1m\]jobs:\j\[\e[32;1m\]) - (\[\e[37;1m\]\w\[\e[32;1m\]) - (\[\e[37;0m\]\!\[\e[32;1m\]) \n----> \[\e[0m\]"
@@ -8,4 +8,3 @@ HISTCONTROL=ignoreboth
 
 # Good general aliases
 alias py='python'
-
